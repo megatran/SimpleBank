05d0e7faa9345babeecc79e1931f5a8c20a1d467
Nhan Tran <nhantrantnt@gmail.com>
2015-01-24 16:41:20 -0700

    Add printHelpMenu, printPrompt, and other "user instruction". User can type "help" to access the help menu or type "quit" to exit.

--- MegatranSimpleBank/src/com/megatran/training/SimpleBank.java
+++ MegatranSimpleBank/src/com/megatran/training/SimpleBank.java
@@ -1,9 +1,26 @@
 package com.megatran.training;
 
+import java.util.Scanner;
+
 public class SimpleBank {
 	public static void main (String[] args) {
 		SimpleBank bank = new SimpleBank();
-		bank.printWelcomeMsg()
+		bank.printWelcomeMsg();
+		Scanner scanner = new Scanner (System.in);
+		do {
+			bank.printPrompt();
+			String commandStr = scanner.nextLine();
+			if (bank.shouldQuit(commandStr))
+				break;
+			if (commandStr.trim().equalsIgnoreCase("help")) {
+				bank.printHelpMenu();
+			}
+		} while(true);
+		System.out.println("Thanks for using the system.");
+	}
+	
+	private void printPrompt() {
+		System.out.print(">");
 	}
 	
 	private void printWelcomeMsg() {
@@ -13,6 +30,16 @@ public class SimpleBank {
 	
 	private void printHelpMenu() {
 		System.out.println("You can do the following operations...");
-		System.out.println("open <amount> - )
+		System.out.println("open <amount> - to open a new account." );
+		System.out.println("\tAn accound number will be printed which you must remember. ");
+		System.out.println("close <accountNumber> - to close an existing account. ");
+		System.out.println("deposit <accountNumber> <amount> - to deposit money in an account");
+		System.out.println("withdraw <accountNumber> <amount> - to withdraw money from an account");
+		System.out.println("balance <accountNumber> - for balance enquiry");
+		System.out.println("help - to print this help");
+		System.out.println("quit - to close the application");
+	}
+	private boolean shouldQuit(String command) {
+		return command.trim().equalsIgnoreCase("quit");
 	}
 }
