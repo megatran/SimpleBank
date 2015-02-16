4cb63f7610f5829796350424a352d0f2ff499da1
Nhan Tran <nhantrantnt@gmail.com>
2015-02-16 00:12:12 -0700

    Create members and methods for BankAccount class

--- MegatranSimpleBank/src/com/megatran/training/BankAccount.java
+++ MegatranSimpleBank/src/com/megatran/training/BankAccount.java
@@ -2,4 +2,25 @@ package com.megatran.training;
 
 public class BankAccount {
 
+	private int accountNumber;
+	private double balance;
+	
+	public BankAccount(int accNum, double initialDeposit) {
+		this.accountNumber = accNum;
+		this.balance = initialDeposit;
+		
+	}
+	public void deposit(double howMuch) {
+		this.balance += howMuch;
+	}
+	public void withdraw(double howMuch) {
+		this.balance -= howMuch;
+	}
+	public int getAccountNumber() {
+		return this.accountNumber;
+	}
+	
+	public double getBalance() {
+		return this.balance;
+	}
 }
